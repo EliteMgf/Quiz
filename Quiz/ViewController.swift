8407b4f473fe6d96da1fe314eb1b89aea8af05a0
John Forester <jforester@ghostrider.highpoint.edu>
2019-01-29 22:38:32 -0500

    Added 5 more questions, High Point University in comments in ViewController.swift

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -4,7 +4,7 @@
 //
 //  Created by John Forester on 1/23/19.
 //  Copyright © 2019 John Forester. All rights reserved.
-//
+//  High Point University
 
 import UIKit
 
@@ -17,11 +17,22 @@ class ViewController: UIViewController {
         "What is 7+7?",
         "What is the capital of Vermont?",
         "What is cognac made from?"
+        "What college has the best college basketball program?"
+        "Who are the star players for Duke Men's Basketball?"
+        "Who is the all time point leader for Duke Men's Basketball?"
+        "What is Duke's mascot?"
+        "How many NCAA Championships does Duke Have?"
     ]
     let answers: [String] = [
         "14",
         "Montpelier",
         "Grapes"
+        "Duke"
+        "Zion Williamson and RJ Barrett"
+        "J.J. Redick"
+        "The Blue Devils"
+        "Five"
+        
     ]
     var currentQuestionIndex: Int = 0
     
